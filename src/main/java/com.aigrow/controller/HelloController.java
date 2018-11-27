1bdd02bb7b68d6352aac41a05f51cbf9ed4dd0ab
Laliten <1275158486@qq.com>
2018-11-27 16:17:13 +0800

    add new method sayHello in class HelloController

--- src/main/java/com.aigrow/controller/HelloController.java
+++ src/main/java/com.aigrow/controller/HelloController.java
@@ -4,5 +4,7 @@ public class HelloController {
     public HelloController(){
         System.out.println("hello world");
     }
-
+    public void sayHello(){
+        System.out.println("sayHello");
+    }
 }
