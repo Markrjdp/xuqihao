9f776476904e1f06e5599c4032fbc94c632d7c02
Laliten <1275158486@qq.com>
2018-11-27 16:20:13 +0800

    添加HelloController

--- src/main/java/com.aigrow/controller/HelloController.java
+++ src/main/java/com.aigrow/controller/HelloController.java
@@ -4,5 +4,7 @@ public class HelloController {
     public HelloController(){
         System.out.println("hello world");
     }
-
+    public static void  main(String[] args){
+        new HelloController();
+    }
 }
